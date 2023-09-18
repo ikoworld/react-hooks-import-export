b508b880a8aa1a39ee7a33c1d8b55ca561fe305b
aboura <bouchrabichou638@gmail.com>
2023-09-18 14:32:55 +0200

    completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
+
+ function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(trees);
+  // => "Aspen and Pine"
+  
+  wildlife();
+  console.log(RMFunctions.trees);
+// => "Aspen and Pine"
 
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+RMFunctions.elevation();
   return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
